aae86d1d39439449c3a90c2af0395a0fc39438d1
abdurcse089 <arsajib7107@gmail.com>
2020-06-03 20:40:24 -0700

    power-x-gym project

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Navbar from './components/Navbar/Navbar';
+import About from './components/AboutUs/About';
+import Workout from './components/Workout/Workout';
+import Training from './components/Training/Training';
+import ChooseUs from './components/ChooseUs/ChooseUs';
+import Footer from './components/Footer/Footer';
+import Classes from './components/Classes/Classes';
+import Notfound from './components/Notfound/Notfound';
+import ProductDetails from './components/ProductDetails/ProductDetails';
+import Pricing from './components/Pricing/Pricing';
+
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/ourClasses">
+          <Navbar></Navbar>
+          <Classes></Classes>
+          <Footer></Footer>
+        </Route>
+        <Route path="/productDetails/:category">
+          <Navbar></Navbar>
+          <ProductDetails></ProductDetails>
+          <Footer></Footer>
+        </Route>
+        <Route path="/pricing">
+          <Navbar></Navbar>
+          <Pricing></Pricing>
+          <Footer></Footer>
+        </Route>
+        <Route path="/">
+          <Navbar></Navbar>
+          <Workout></Workout>
+          <About></About>
+          <Training></Training>
+          <ChooseUs></ChooseUs>
+          <Footer></Footer>
+        </Route>
+        <Route path="*">
+          <Notfound></Notfound>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Footer.scss';
+import youtube from '../../Images/bxl-youtube.png';
+import fb from '../../Images/bxl-facebook.png';
+import instra from '../../Images/bxl-instagram.png';
+import twiter from '../../Images/bxl-twitter.png';
+import whatsapp from '../../Images/bxl-whatsapp.png';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-2">
+                        <a className="text-white" href="/">POWER <span className="logoX">X</span></a>
+                    </div>
+                    <div className="col-md-3">
+                    <h5><a className="text-white" href="https://github.com/abdurcse089">Need Help?</a></h5>
+                    <h5><a className="text-white" href="https://github.com/abdurcse089">Help Center</a></h5>
+                    <h5><a className="text-white" href="https://github.com/abdurcse089">Email Support</a></h5>
+                    <h5><a className="text-white" href="https://github.com/abdurcse089">Live Chat</a></h5>
+                    <h5><a className="text-white" href="https://github.com/sabdurcse089">Gift Certificates</a></h5>
+                    <h5><a className="text-white" href="https://github.com/abdurcse089">Send Us Feedback</a></h5> 
+
+                    </div>
+                    <div className="col-md-2 ">
+                        <h5><a className="text-white" href="https://github.com/abdurcse089">Digital Resources</a></h5>
+                        <h5><a className="text-white" href="https://github.com/abdurcse089">Articles</a></h5>
+                        <h5><a className="text-white" href="https://github.com/abdurcse089">E-books</a></h5>
+                    </div>
+                    <div className="col-md-2 text-white socialLogo">
+                        <h5>Connect with Us</h5>
+                        <a href="https://github.com/abdurcse089"><img src={youtube}alt=""/></a>
+                        <a href="https://github.com/abdurcse089"><img src={fb} alt=""/></a>
+                        <a href="https://github.com/abdurcse089"><img src={instra} alt=""/></a>
+                        <a href="https://github.com/abdurcse089"><img src={twiter} alt=""/></a>
+                        <a href="https://github.com/abdurcse089"><img src={whatsapp} alt=""/></a>
+                        <a className="text-white" href="https://github.com/abdurcse089">Forums</a>
+                    </div>
+                    <div className="col-md-2 text-white ">
+                        <h5>Join Our Newsletter</h5>
+                        <h6>Get exclusive news, features, and updates from the Shredder Weight Loss Academy</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
